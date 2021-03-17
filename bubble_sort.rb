49bb1df933d03170078eb0aaf23c7aba4771f0cf
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-17 20:01:28 -0300

    First working version of bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,18 @@
 def bubble_sort(array) 
   i = 0
   j = 1
-  sorted? = true
-  while sorted? 
+  sorted = true
+  while sorted 
+    swapped = 0
     while i < array.length - 1 
       if array[i] > array[j]
         array[i], array[j] = array[j], array[i]
+        swapped += 1
       end
       i += 1
       j += 1
     end
+    sorted = false if swapped == 0
   end
   array
 end 

300432714d29ee422cc8d10ac10fc8ce128856d8
Marco <marcoparra311@gmail.com>
2021-03-18 11:54:59 -0400

    Begin to implement bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,30 @@ def bubble_sort(array)
   end
   array
 end
+
+def bubble_sort_by(array)
+  i = 0
+  sorted = false
+  until sorted
+    swapped = 0
+    while i < array.length - 1
+      orientation = yield(array[i], array[i + 1])
+      if orientation.positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped += 1
+      elsif orientation.negative?
+        array[i + 1], array[i]  = array[i], array[i + 1]
+        swapped += 1
+      end
+      i += 1
+    end
+    sorted = true if swapped.zero?
+    i = 0
+  end
+   p array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end 
+

a3bcf4eb8ca62cd386cff11353837301fa5111f6
Marco <marcoparra311@gmail.com>
2021-03-18 11:17:20 -0400

    Try to implement logic comparing elements and change it back to the original logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,17 @@
 def bubble_sort(array)
-  array.each_index do |i|
-    array.each_index do |j|
-      array[i], array[j] = array[j], array[i] if array[i] < array[j]
+  i = 0
+  sorted = false
+  until sorted
+    swapped = 0
+    while i < array.length - 1
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped += 1
+      end
+      i += 1
     end
+    sorted = true if swapped.zero?
+    i = 0
   end
   array
 end
-
-

abd9ff9b33c61e588e4a8828e8b6aa2b514e58ac
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-18 11:12:35 -0300

    Add changes to comply with standup team issues report - inverting while-until logic and transforming j into i + 1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,17 @@
 def bubble_sort(array) 
   i = 0
-  j = 1
-  sorted = true
-  while sorted 
+  sorted = false
+  until sorted 
     swapped = 0
     while i < array.length - 1 
-      if array[i] > array[j]
-        array[i], array[j] = array[j], array[i]
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped += 1
       end
       i += 1
-      j += 1
     end
-    sorted = false if swapped == 0
+    sorted = true if swapped == 0
     i = 0
-    j = 1
   end
   array
 end 
